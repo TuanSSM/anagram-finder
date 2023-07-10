b3edc671f14a48f8ddcac12e752d4a84e103f1b4
tuan <tuan@tuanssm.xyz>
2023-07-10 11:47:33 +0300

    mongo k8s files

--- cmd/anagram-finder/main.go
+++ cmd/anagram-finder/main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/tuanssm/anagram-finder/internal/api"
 )
 
 func main() {
-	//listenAddr := ":" + os.Getenv("ANAGRAM_FINDER_API_PORT")
-	//mongoURI := os.Getenv("MONGO_URI")
-	listenAddr := ":8080"
-	mongoURI := "mongodb://0.0.0.0:27017"
+	listenAddr := ":" + os.Getenv("ANAGRAM_FINDER_API_PORT")
+	mongoURI := os.Getenv("MONGO_URI")
 
 	apiServer := api.NewApiServer()
 	log.Fatal(apiServer.Start(listenAddr, mongoURI))
